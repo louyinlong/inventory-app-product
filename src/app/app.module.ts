a0d8367ded053cc48d9080e95e07f75582850750
louyinlong <1092755001@qq.com>
2020-04-10 14:54:14 +0800

    product
    Signed-off-by: louyinlong <1092755001@qq.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Student } from './student.component';
-import { StudentList } from './student-list.component';
+import { Product } from './product';
+import { ProductsListComponent } from './products-list.component';
+import { ProductDepartmentComponent } from './product-department.component';
+import { ProductImageComponent } from './product-image.component';
+import { ProductPriceComponent } from './product-price.component';
+import { ProductRowComponent } from './product-row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentList,
-    Student
+    ProductsListComponent,
+    ProductDepartmentComponent,
+    ProductImageComponent,
+    ProductPriceComponent,
+    ProductRowComponent
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/product-row.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Product } from './product';
+
+@Component({
+    selector: 'product-row',
+    templateUrl: 'product-row.component.html',
+    styleUrls: ['product-row.component.css'],
+    inputs: ['product'],
+    host: { class: 'row' }
+})
+
+export class ProductRowComponent {
+    product: Product;
+
+    constructor() {
+
+    }
+}
